315e6c833d51a383d467a73915168b975904d428
ulyh <janeuly@pm.me>
2026-06-20 07:18:21 +0200

    EDIT main method add switch case

--- main.cpp
+++ main.cpp
@@ -2,19 +2,26 @@
 #include <iostream>
 
 int main(void){
-    // std::cout << "PARTIE 1 : CHIFFRE ET NON SAUVEGARDE\n";
-    // Substitution sub = Substitution(); // mode CHIFFRE et analyse non sauvegardée par défaut
-    // sub.Saisie();
-    // sub.Sortie();
-
-    std::cout << "PARTIE 1 : CHIFFRE ET SAUVEGARDE\n";
-    Substitution sub1 = Substitution(MODE::CHIFFRE, true); 
-    sub1.Saisie();
-    sub1.Sortie();
-
-    // std::cout << "PARTIE 2 : DECHIFFRE ET NON SAUVEGARDE\n";
-    // Substitution sub0 = Substitution(MODE::DECHIFFRE); // pas de sauvegarde possible dans cette partie du projet
-    // sub0.Saisie();
-    // sub0.Calcule_clé(); 
-    // sub0.Sortie();
+    Substitution sub0, sub1, sub2;
+    int cas = 0;
+    switch (cas){
+    case 1:
+        std::cout << "PARTIE 1 : CHIFFRE ET SAUVEGARDE\n";
+        sub1 = Substitution(MODE::CHIFFRE, true); 
+        sub1.Saisie();
+        sub1.Sortie();
+        break;
+    case 2:
+        std::cout << "PARTIE 2 : DECHIFFRE ET NON SAUVEGARDE\n";
+        sub2 = Substitution(MODE::DECHIFFRE); // pas de sauvegarde possible dans cette partie du projet
+        sub0.Saisie();
+        sub0.Calcule_clé(); 
+        sub0.Sortie();
+        break;
+    default:
+        std::cout << "PARTIE 1 : CHIFFRE ET NON SAUVEGARDE\n";
+        Substitution sub = Substitution(); // mode CHIFFRE et analyse non sauvegardée par défaut
+        sub.Saisie();
+        sub.Sortie();
+    }
 }
